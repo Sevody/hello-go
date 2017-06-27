d5fc51238e26867c2fd069177402f9ff41201616
sevody <sevody@gmail.com>
2017-06-27 22:07:38 +0800

    finish 12.2

---
+++ solution/12.4/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io/ioutil"
+	"fmt"
+)
+
+
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (this *Page) save() (err error) {
+	return ioutil.WriteFile(this.Title, this.Body, 0666)
+}
+
+func (this *Page) load(title string) (err error) {
+	this.Title = title
+	this.Body, err = ioutil.ReadFile(this.Title)
+	return err
+}
+
+func main() {
+	page := Page{
+		"Page.md",
+		[]byte("# Page\n## Section1\nThis is section1."),
+	}
+	page.save()
+
+	var new_page Page
+	new_page.load("Page.md")
+	fmt.Println(string(new_page.Body))
+}
